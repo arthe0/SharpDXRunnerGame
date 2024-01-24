d001750946228a96269dd5480ad999dc369c4f77
QUANTUMPEPE <wasamk79@gmail.com>
2024-01-24 22:09:05 +0300

    Initial commit

---
+++ Content/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Engine;
+using Engine.BaseAssets.Components;
+
+namespace Game1.Content.Scripts
+{
+    public class TestComponent : BehaviourComponent
+    {
+        [SerializedField] private double Value = 1;
+        
+        public override void Start()
+        {
+            base.Start();
+            Logger.Log(LogType.Info, $"Component started with Value = {Value}");           
+        }
+    }
+}
+
