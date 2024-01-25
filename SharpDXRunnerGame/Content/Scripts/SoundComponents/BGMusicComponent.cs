419fd4b4adef51e8772da13ea351cd18d785a7c0
QUANTUMPEPE <wasamk79@gmail.com>
2024-01-26 01:33:14 +0300

    Add sounds

---
+++ SharpDXRunnerGame/Content/Scripts/SoundComponents/BGMusicComponent.cs
@@ -0,0 +1,15 @@
+﻿using Engine;
+using Engine.BaseAssets.Components;
+
+namespace SharpDXRunnerGame.Content.Scripts.SoundComponents;
+
+public class BGMusicComponent : BehaviourComponent
+{
+    [SerializedField]
+    private Sound music = null;
+
+    public override void Start()
+    {
+        SoundCore.Play(music);
+    }
+}
